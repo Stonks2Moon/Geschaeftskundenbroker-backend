b5e4e6c27dd2e0c4f0cc903dd83ce8c9d3fe6b95
Benno Grimm <43752927+Grimmig18@users.noreply.github.com>
2021-03-04 20:33:35 +0100

    Add test api endpoint

--- server/src/costumer/costumer.controller.ts
+++ server/src/costumer/costumer.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CostumerSession } from './costumer-session.model';
 import { CostumerService } from './costumer.service';
 
@@ -16,5 +16,10 @@ export class CostumerController {
         @Body('session') session?: CostumerSession
     ) {
         return await this.costumerService.costumerLogin(login, session);
+    }
+
+    @Get('test')
+    async testCall() {
+        return "Hello World";
     }
 }
