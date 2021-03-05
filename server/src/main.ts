12e77db2832e6e95b6eabecde430c1d40b84ac14
NerdyStuff <NerdyStuff@users.noreply.github.com>
2021-03-05 20:28:04 +0100

    Add CORS allow all

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      /^(.*)/,
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders:
+      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
+  })
   await app.listen(3000);
 }
 bootstrap();
