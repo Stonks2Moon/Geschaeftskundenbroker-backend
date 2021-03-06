2afe24c4877d20cff7d277ea7341242cd3fc104c
Benno Grimm <43752927+Grimmig18@users.noreply.github.com>
2021-03-06 19:04:05 +0100

    Add share endpoint and files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
 import { CompanyModule } from './company/company.module';
+import { ShareController } from './share/share.controller';
+import { ShareService } from './share/share.service';
+import { ShareModule } from './share/share.module';
 
 @Module({
-  imports: [CompanyModule, CustomerModule],
+  imports: [CompanyModule, CustomerModule, ShareModule],
+  controllers: [ShareController],
+  providers: [ShareService],
 })
 export class AppModule {}
---
+++ server/src/share/share.model.ts
@@ -0,0 +1,9 @@
+export interface Share {
+    shareId?: number
+    sharName: string,
+    isin: string,
+    wkn: string,
+    lastRecordedValue: number,
+    currencyCode: string,
+    currencyName: string
+}
