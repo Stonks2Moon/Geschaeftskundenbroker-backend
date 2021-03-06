9bfd3bdbfd3c74f2d54f3b080feff866c0c707c9
NerdyStuff <NerdyStuff@users.noreply.github.com>
2021-03-06 19:08:30 +0100

    Add services to ShareModule

--- server/src/share/share.module.ts
+++ server/src/share/share.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ShareController } from './share.controller';
+import { ShareService } from './share.service';
 
-@Module({})
+@Module({
+    controllers: [ShareController],
+    providers: [ShareService],
+    exports: [ShareService]
+})
 export class ShareModule {}
