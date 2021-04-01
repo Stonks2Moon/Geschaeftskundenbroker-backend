e8676d7e47c8dce885667e3ec7558ad9a337d9db
Benno Grimm <43752927+Grimmig18@users.noreply.github.com>
2021-04-01 09:22:08 +0200

    Add swagger schema to meta info

--- server/src/meta/meta.controller.ts
+++ server/src/meta/meta.controller.ts
@@ -1,9 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBody, ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
 import * as StaticConsts from '../util/static-consts';
 
 @Controller('meta')
 export class MetaController {
     
+    @ApiTags('')
+    @ApiResponse({
+        schema: {
+            properties: {
+                'DEFAULT_SEARCH_LIMIT': { type: 'number' },
+                'ALG_SPLIT_THRESHOLD': { type: 'number' },
+                'ALGORITHMS': {
+                    type: 'object',
+                    properties: {
+                        'NO_ALG': { type : 'number' },
+                        'SPLIT_ALG': { type : 'number' },
+                    }
+                }
+            }
+        }
+    })
     @Get('')
     async exportStaticConsts() {
         return {
