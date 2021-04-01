b9ac965ed5ea628a1c83a84efefeb901916d64aa
Benno Grimm <43752927+Grimmig18@users.noreply.github.com>
2021-04-01 09:01:40 +0200

    Export static consts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { CompanyModule } from './company/company.module';
 import { ShareModule } from './share/share.module';
 import { DepotModule } from './depot/depot.module';
 import { UpdateShares} from './util/stock-exchange/share-updates';
+import { MetaModule } from './meta/meta.module';
 
 @Module({
-  imports: [CompanyModule, CustomerModule, ShareModule, DepotModule],
+  imports: [CompanyModule, CustomerModule, ShareModule, DepotModule, MetaModule],
   providers: [UpdateShares]
 })
 export class AppModule { }
--- server/src/util/static-consts.ts
+++ server/src/util/static-consts.ts
@@ -12,3 +12,7 @@ export const STOCK_EXCHANGE_API_URL = "ws://boerse.moonstonks.space:16001";
 /*------------- ALGORITHM -------------*/
 export const ALG_SPLIT_THRESHOLD = 10_000;
 export const ALG_SPLIT_BATCH_VALUE = 5_000;
+export const ALGORITHMS = {
+    NO_ALG: 0,
+    SPLIT_ALG: 1
+}
